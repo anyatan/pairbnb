26d22445a74d46beac1e055d169ce278091d39d0
anyatan <flower_tzeee@hotmail.com>
2016-01-20 14:41:34 +0800

    remove elastic search config'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,9 +58,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # elastic search 
-  # elasticsearch_url = ENV['SEARCHBOX_URL'] || ENV['SEARCHLY_URL'] ||
-  #   JSON.parse(ENV['VCAP_SERVICES']['searchly'][0]['credentials']['uri']) || 'http://site:xyz-searchly.com'
-
-  # Elasticsearch::Model.client = Elasticsearch::Client.new host: elasticsearch_url
 end
