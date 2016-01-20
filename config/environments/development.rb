16bbda81760f63cb972bea4f4f660c9c5a58aaef
anyatan <flower_tzeee@hotmail.com>
2016-01-20 14:39:10 +0800

    .

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,4 +57,10 @@ Rails.application.configure do
     end
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # elastic search 
+  # elasticsearch_url = ENV['SEARCHBOX_URL'] || ENV['SEARCHLY_URL'] ||
+  #   JSON.parse(ENV['VCAP_SERVICES']['searchly'][0]['credentials']['uri']) || 'http://site:xyz-searchly.com'
+
+  # Elasticsearch::Model.client = Elasticsearch::Client.new host: elasticsearch_url
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  #elastic search 
+  elasticsearch_url = ENV['SEARCHBOX_URL'] || ENV['SEARCHLY_URL'] ||
+    JSON.parse(ENV['VCAP_SERVICES']['searchly'][0]['credentials']['uri']) || 'http://site:xyz-searchly.com'
+
+  Elasticsearch::Model.client = Elasticsearch::Client.new host: elasticsearch_url
 end
